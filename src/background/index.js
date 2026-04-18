ed37988e0944b203fa2b94ac0fe2fc551ba33583
chiameixi <chiameixi@gmail.com>
2026-04-18 17:16:13 -0400

    first commit

---
+++ src/background/index.js
@@ -0,0 +1,2 @@
+(function(){"use strict";chrome.sidePanel.setPanelBehavior({openPanelOnActionClick:!0}).catch(()=>{}),chrome.runtime.onMessage.addListener((e,n,s)=>{})})();
+//# sourceMappingURL=index.js.map
